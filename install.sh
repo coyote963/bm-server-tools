f37b7fa24756bf89bb733d63307727371e0deb70
c b <coyoteandbird@gmail.com>
2021-10-30 18:55:55 -0400

    adds bash install script

--- install.sh
+++ install.sh
@@ -1,34 +1,65 @@
+#!/bin/bash
 
-echo "(Re)install docker? Choose yes if unsure. (y/n):"
+BASEPATH=$HOME
+CONDA_URL=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
+install_miniconda() {
+  curl -k $CONDA_URL >${BASEPATH}/conda_install.sh
+  bash ${BASEPATH}/conda_install.sh -b -p ${BASEPATH}/Miniconda3
+  rm -f ${BASEPATH}/conda_install.sh
+  $BASEPATH/miniconda3/bin/conda update -y -n base -c defaults conda
+}
 
+install_tui() {
+  $BASEPATH/miniconda3/bin/conda create -y -n bm-tui python=3.7
+  source $BASEPATH/miniconda3/bin/activate bm-tui
+  conda install --file ./src/tui/requirements.txt
+}
+
+install_cli() {
+  $BASEPATH/miniconda3/bin/conda create -y -n bm-cli python=3.7
+  source $BASEPATH/miniconda3/bin/activate bm-cli
+  conda install --file ./src/cli/requirements.txt
+}
+
+if [[! -d "$BASEPATH/miniconda3" ]]; then
+  install_miniconda
+fi
 
 install_docker() {
-  sudo apt-get remove docker docker-engine docker.io containerd-runc
-  sudo apt-get update
-  sudo apt-get install \
-      ca-certificates \
-      curl \
-      gnupg \
-      lsb-release
-  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-  echo \
-    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt-get update
-  sudo apt-get install docker-ce docker-ce-cli containerd.io
+  curl -fsSL get.docker.com -o get-docker.sh
+  sudo sh get-docker.sh
 }
 
-install_dependencies() {
-  echo "Installing dependencies"
-}
-
-
-while true; do
-  read -p "Do you wish to install this docker? Choose yes if unsure (y/n): " yn
+echo "Do you wish to install docker? [1-2]"
+select yn in "Yes" "No"; do
   case $yn in
-    [Yy]* ) install_docker; break;;
-    [Nn]* ) break;;
-    * ) echo "Please answer y or n.";;
+  Yes)
+    install_docker
+    break
+    ;;
+  No) break ;;
   esac
+  echo "Not a valid choice [1, 2]"
+done
+
+echo "Which component do you want to install? [1-4]"
+select opt in "CLI" "TUI" "ALL" "NONE"; do
+  case $opt in
+  CLI)
+    install_cli
+    break
+    ;;
+  TUI)
+    install_tui
+    break
+    ;;
+  ALL)
+    install_cli
+    install_tui
+    break
+    ;;
+  NONE) break ;;
+  esac
+  echo "Not a valid choice [1, 2, 3, 4]"
 done
